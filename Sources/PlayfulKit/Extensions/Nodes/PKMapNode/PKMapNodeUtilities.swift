068356163a686ae785eb66503bdba672f7b7b246
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-02-17 22:22:31 +0100

    method tileNode(with name) added to PKMapNode extension

--- Sources/PlayfulKit/Extensions/Nodes/PKMapNode/PKMapNodeUtilities.swift
+++ Sources/PlayfulKit/Extensions/Nodes/PKMapNode/PKMapNodeUtilities.swift
@@ -11,4 +11,8 @@ public extension PKMapNode {
     var tiles: [PKTileNode] {
         return self.children.compactMap { $0 as? PKTileNode }
     }
+    
+    func tileNode(with coordinate: PKCoordinate) -> PKTileNode? {
+        tiles.first(where: { $0.coordinate == coordinate })
+    }
 }
