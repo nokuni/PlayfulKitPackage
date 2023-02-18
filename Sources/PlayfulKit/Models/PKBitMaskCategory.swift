37235d778b9e053ca6a80c5a029a3936c222651c
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-02-18 15:04:09 +0100

    modified bits

--- Sources/PlayfulKit/Models/PKBitMaskCategory.swift
+++ Sources/PlayfulKit/Models/PKBitMaskCategory.swift
@@ -10,11 +10,11 @@ import Foundation
 public enum PKBitMaskCategory: UInt32 {
     case allClear         = 0x00000000
     case allSet           = 0xFFFFFFFF
-    case player           = 2
-    case playerProjectile = 4
-    case enemy            = 6
-    case enemyProjectile  = 8
-    case object           = 10
-    case wall             = 12
-    case ground           = 14
+    case player           = 0x00000002
+    case playerProjectile = 0x00000004
+    case enemy            = 0x00000006
+    case enemyProjectile  = 0x00000008
+    case object           = 0x00000010
+    case wall             = 0x00000012
+    case ground           = 0x00000014
 }
