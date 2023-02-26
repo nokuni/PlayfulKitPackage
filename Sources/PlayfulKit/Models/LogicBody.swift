868d8edf804bfebaf60b606d0116ecd2eb77b59c
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-02-26 18:48:12 +0100

    public init logic body

--- Sources/PlayfulKit/Models/LogicBody.swift
+++ Sources/PlayfulKit/Models/LogicBody.swift
@@ -9,8 +9,18 @@ import Foundation
 
 /// The logic of an object.
 public struct LogicBody {
-    public var health: Int = 1
-    public var healthLost: Int = 0
-    public var damage: Int = 0
-    public var isDestructible: Bool = false
+    public init(health: Int = 1,
+                healthLost: Int = 0,
+                damage: Int = 0,
+                isDestructible: Bool = false) {
+        self.health = health
+        self.healthLost = healthLost
+        self.damage = damage
+        self.isDestructible = isDestructible
+    }
+    
+    public var health: Int
+    public var healthLost: Int
+    public var damage: Int
+    public var isDestructible: Bool
 }
