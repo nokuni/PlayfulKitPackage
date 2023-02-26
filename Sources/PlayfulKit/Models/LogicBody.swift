72f72c02ae4f75a2ff78f1d519ab9cf32842dd28
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-02-26 16:02:48 +0100

    de-comment and modify logic body for object node. For now, isDestructible is the only parameter.

--- Sources/PlayfulKit/Models/LogicBody.swift
+++ Sources/PlayfulKit/Models/LogicBody.swift
@@ -8,9 +8,6 @@
 import Foundation
 
 /// The logic of an object.
-//public struct LogicBody: Codable {
-//    public var health: Int
-//    public var healthLost: Int = 0
-//    public var damage: Int
-//    public var isDestructible: Bool
-//}
+public struct LogicBody: Codable {
+    public var isDestructible: Bool = false
+}
