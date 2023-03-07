dcf3b33d33b74bfb20ba99f389cc36ac079835ec
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-03-07 14:10:23 +0100

    Fix on game controllers

--- Sources/PlayfulKit/Managers/ControllerManager.swift
+++ Sources/PlayfulKit/Managers/ControllerManager.swift
@@ -43,7 +43,17 @@ public class ControllerManager {
             }
         }
     }
-    public struct DirectionPadAction {
+    
+    public struct ButtonAction {
+        public init(press: (() -> Void)? = nil, release: (() -> Void)? = nil) {
+            self.press = press
+            self.release = release
+        }
+        
+        public var press: (() -> Void)?
+        public var release: (() -> Void)?
+    }
+    public struct DPadAction {
         public  init(left: @escaping (() -> Void),
                      right: @escaping (() -> Void),
                      up: @escaping (() -> Void),
@@ -60,79 +70,140 @@ public class ControllerManager {
         var down: (() -> Void)
     }
     
-    public var scene: SKScene
-    
-    public var buttonMenu: (() -> Void)?
-    public var buttonA: (() -> Void)?
-    public var buttonB: (() -> Void)?
-    public var buttonX: (() -> Void)?
-    public var buttonY: (() -> Void)?
-    public var padAction: DirectionPadAction?
-    public var virtualControllerElements: [VirtualControllerElement] = []
+    public struct ControllerAction {
+        public init(buttonMenu: ButtonAction = ButtonAction(),
+                    buttonA: ButtonAction = ButtonAction(),
+                    buttonB: ButtonAction = ButtonAction(),
+                    buttonX: ButtonAction = ButtonAction(),
+                    buttonY: ButtonAction = ButtonAction(),
+                    dpad: DPadAction? = nil) {
+            self.buttonMenu = buttonMenu
+            self.buttonA = buttonA
+            self.buttonB = buttonB
+            self.buttonX = buttonX
+            self.buttonY = buttonY
+            self.dpad = dpad
+        }
+        
+        public var buttonMenu: ButtonAction
+        public var buttonA: ButtonAction
+        public var buttonB: ButtonAction
+        public var buttonX: ButtonAction
+        public var buttonY: ButtonAction
+        public var dpad: DPadAction?
+    }
     
     private var virtualController: GCVirtualController?
+    
+    public var scene: SKScene
+    public var action: ControllerAction?
+    public var virtualControllerElements: [VirtualControllerElement] = []
+    
+    /// Observe the controllers and establish a connexion.
+    public func observeControllers() {
+        NotificationCenter.default.addObserver(forName: .GCControllerDidConnect,
+                                               object: nil,
+                                               queue: nil) { notification in
+            self.connectController(notification)
+        }
+        NotificationCenter.default.addObserver(forName: .GCControllerDidDisconnect,
+                                               object: nil,
+                                               queue: nil) { notification in
+            self.disconnectController(notification)
+        }
+        
+        setupVirtualController()
+        
+        guard let controller = GCController.controllers().first else { return }
+        
+        register(controller)
+    }
+    
+    // MARK: - Setup
+    private func connectController(_ notification: Notification) {
+        guard let controller = notification.object as? GCController else { return }
+        unregister()
+        disconnectVirtualController(controller)
+        register(controller)
+    }
+    private func disconnectController(_ notification: Notification) {
+        unregister()
+        connectVirtualController()
+    }
+    private func unregister() {
+        
+    }
+    
+    // MARK: - Virtual
     private var virtualControllerElementNames: Set<String> {
         let names = virtualControllerElements.map { $0.name }
         let elements = Set(names)
         return elements
     }
-    
-    public func observeControllers() {
-        NotificationCenter.default.addObserver(self, selector: #selector(connectControllers), name: NSNotification.Name.GCControllerDidConnect, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(disconnectControllers), name: NSNotification.Name.GCControllerDidDisconnect, object: nil)
+    private var virtualControllerConfiguration: GCVirtualController.Configuration {
+        let configuration = GCVirtualController.Configuration()
+        configuration.elements = virtualControllerElementNames
+        return configuration
     }
-    
-    @objc private func connectControllers(notification: Notification) {
-        scene.isPaused = false
-        if let controller = notification.object as? GCController {
-            setupControllerControls(controller: controller)
-        } else {
-            connectVirtualController()
-        }
+    private func setupVirtualController() {
+        virtualController = GCVirtualController(configuration: virtualControllerConfiguration)
+        connectVirtualController()
+        registerVirtualInputs()
     }
-    @objc private func disconnectControllers(notification: Notification) {
-        scene.isPaused = true
-        if GCController.controllers().isEmpty {
-            connectVirtualController()
-        }
-    }
-    
     private func connectVirtualController() {
-        // Connect to the virtual controller if no physical controllers are available.
-        if GCController.controllers().isEmpty {
-            let virtualConfiguration = GCVirtualController.Configuration()
-            virtualConfiguration.elements = virtualControllerElementNames
-            virtualController = GCVirtualController(configuration: virtualConfiguration)
-            virtualController?.connect()
+        guard GCController.controllers().isEmpty else { return }
+        virtualController?.connect()
+    }
+    private func disconnectVirtualController(_ controller: GCController) {
+        if controller != virtualController?.controller {
+            virtualController?.disconnect()
         }
     }
     
-    // MARK: - Controller Controls
-    private func setupControllerControls(controller: GCController) {
-        controller.extendedGamepad?.valueChangedHandler = {
+    // MARK: - Controls
+    private func register(_ controller: GCController?) {
+        controller?.extendedGamepad?.valueChangedHandler = {
             (gamepad: GCExtendedGamepad, element: GCControllerElement) in
-            self.controllerInputDetected(gamepad: gamepad, element: element, index: controller.playerIndex.rawValue)
+            self.input(on: gamepad)
         }
     }
-    private func pressButton(_ button: GCControllerButtonInput, action: (() -> Void)) {
-        if button.isPressed { action() }
+    private func pressButton(_ button: GCControllerButtonInput, action: ButtonAction?) {
+        if button.isPressed { action?.press?() } else { action?.release?() }
     }
     private func pressDpad(_ directionPad: GCControllerDirectionPad,
-                                action: DirectionPadAction?) {
+                           action: DPadAction?) {
         if directionPad.right.isPressed && !directionPad.left.isPressed { action?.left() }
         if directionPad.left.isPressed && !directionPad.right.isPressed { action?.right() }
         
         if directionPad.up.isPressed && !directionPad.down.isPressed { action?.up() }
         if directionPad.down.isPressed && !directionPad.up.isPressed { action?.down() }
     }
-    private func controllerInputDetected(gamepad: GCExtendedGamepad,
-                                         element: GCControllerElement,
-                                         index: Int) {
-        pressButton(gamepad.buttonMenu) { buttonMenu?() }
-        pressButton(gamepad.buttonA) { buttonA?() }
-        pressButton(gamepad.buttonB) { buttonB?() }
-        pressButton(gamepad.buttonX) { buttonX?() }
-        pressButton(gamepad.buttonY) { buttonY?()}
-        pressDpad(gamepad.dpad, action: padAction)
+    private func input(on gamepad: GCExtendedGamepad) {
+        pressButton(gamepad.buttonMenu, action: action?.buttonMenu)
+        pressButton(gamepad.buttonA, action: action?.buttonA)
+        pressButton(gamepad.buttonB, action: action?.buttonA)
+        pressButton(gamepad.buttonX, action: action?.buttonA)
+        pressButton(gamepad.buttonY, action: action?.buttonA)
+        pressDpad(gamepad.dpad, action: action?.dpad)
+    }
+    
+    private func registerButton(_ button: GCControllerButtonInput?, action: ButtonAction?) {
+        button?.valueChangedHandler = { button, value, isPressed in
+            self.pressButton(button, action: action)
+        }
+    }
+    private func registerDpad(_ dpad: GCControllerDirectionPad?) {
+        guard let dpad = dpad else { return }
+        dpad.valueChangedHandler = { button, value, isPressed in
+            self.pressDpad(dpad, action: self.action?.dpad)
+        }
+    }
+    private func registerVirtualInputs() {
+        registerDpad(virtualController?.controller?.extendedGamepad?.dpad)
+        
+        registerButton(virtualController?.controller?.extendedGamepad?.buttonA, action: action?.buttonA)
+        registerButton(virtualController?.controller?.extendedGamepad?.buttonB, action: action?.buttonB)
+        registerButton(virtualController?.controller?.extendedGamepad?.buttonX, action: action?.buttonX)
+        registerButton(virtualController?.controller?.extendedGamepad?.buttonY, action: action?.buttonY)
     }
 }
